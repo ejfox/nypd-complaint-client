c724a01507042c07316cc2f0464013781f528bb5
EJ Fox <ejfox@ejfox.com>
2021-11-02 15:22:08 -0400

    feat: add data, show network in q5

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,31 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    <ForceLayout :networkData="networkData" :width="width" :height="height" />
   </div>
 </template>
 
 <script>
+// import * as d3 from "d3";
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+// import HelloWorld from "@/components/HelloWorld.vue";
+import ForceLayout from "@/components/ForceLayout.vue";
+import networkData from "@/data/ingredientUniverse_weight_modularity.r1.json";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld,
+    ForceLayout,
+  },
+  data: function () {
+    return {
+      networkData,
+      width: 1284,
+      height: 1284,
+    };
+  },
+  mounted: function () {
+    this.width = window.innerWidth;
+    this.height = window.innerHeight;
   },
 };
 </script>
